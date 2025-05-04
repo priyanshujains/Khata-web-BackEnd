dbd200d3d1880f4fc56ea63653dc3305674e4128
priyanshujains <jainpriyanshu0129@gmail.com>
2025-05-04 15:57:02 +0530

    Insitialising the project

---
+++ src/main/java/com/example/KhataWeb/KhataWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.KhataWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhataWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhataWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/KhataWeb/KhataWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KhataWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhataWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
