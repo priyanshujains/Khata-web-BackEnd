15d957def747325d73c7cadf4b085265f78cfb0f
ishkasingh <ishka14singh@gmail.com>
2025-05-04 16:53:49 +0530

    Customer Controller

---
+++ src/main/java/com/example/KhataWeb/Contoller/CustomerController.java
@@ -0,0 +1,29 @@
+package com.example.KhataWeb.Contoller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController()
+@RequestMapping("/customer")
+public class CustomerController {
+
+    @GetMapping()
+    public ResponseEntity<?> getAllCustomers(){
+        return new ResponseEntity<>()
+    }
+
+    @PostMapping()
+    public ResponseEntity<?> addCustomer(@ResponseBody
+                                         )
+
+    @GetMapping("/{custId}")
+    public ResponseEntity<?> getProductById(@PathVariable long custId){
+
+    }
+
+    @
+
+
+
+}
