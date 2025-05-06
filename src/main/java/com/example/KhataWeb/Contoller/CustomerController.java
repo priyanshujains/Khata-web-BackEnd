616190ea1a0fd27efb4a40794ab31f7fe606be5b
ishkasingh <ishka14singh@gmail.com>
2025-05-06 15:51:56 +0530

    Customer servide added

--- src/main/java/com/example/KhataWeb/Contoller/CustomerController.java
+++ src/main/java/com/example/KhataWeb/Contoller/CustomerController.java
@@ -1,28 +1,72 @@
 package com.example.KhataWeb.Contoller;
 
+import com.example.KhataWeb.Models.Customer;
+import com.example.KhataWeb.Service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController()
-@RequestMapping("/customer")
+@RequestMapping("/api/customer")
 public class CustomerController {
 
-    @GetMapping()
-    public ResponseEntity<?> getAllCustomers(){
-        return new ResponseEntity<>()
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService){
+        this.customerService=customerService;
     }
 
-    @PostMapping()
-    public ResponseEntity<?> addCustomer(@ResponseBody
-                                         )
+    @GetMapping
+    public ResponseEntity<List<Customer>> getAllCustomers(){
+        List<Customer> customers=customerService.getAllCustomers();
+        return new ResponseEntity<>(customers, HttpStatus.OK);
+    }
+
 
     @GetMapping("/{custId}")
-    public ResponseEntity<?> getProductById(@PathVariable long custId){
+    public ResponseEntity<Customer> getProductById(@PathVariable("custId") Long custId){
+        try{
+            Customer customer=customerService.getCustomerById(custId);
+            return new ResponseEntity<>(customer,HttpStatus.OK);
+        }catch(RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
 
+   }
+
+    @PostMapping
+    public ResponseEntity<Customer> addCustomer(@RequestBody Customer customer ){
+        Customer newcustomer=customerService.addCustomer(customer);
+        return new ResponseEntity<>(newcustomer,HttpStatus.CREATED);
     }
 
-    @
+    @PutMapping("/{custId}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable("custId") Long custId, @RequestBody Customer customer){
+        try{
+            Customer updatedCustomer=customerService.updateCustomer(custId,customer);
+            return new ResponseEntity<>(updatedCustomer,HttpStatus.OK);
+        }catch (RuntimeException e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{custId}")
+    public ResponseEntity<?> deleteCustomer(@PathVariable("custId") Long custId){
+        try{
+            customerService.deleteCustomer(custId);
+            return new ResponseEntity<>(HttpStatus.OK);
+
+        }catch (RuntimeException e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+
 
 
 
--- src/main/java/com/example/KhataWeb/Models/Product.java
+++ src/main/java/com/example/KhataWeb/Models/Product.java
@@ -13,9 +13,9 @@ public class Product {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
-
     private String name;
     private Long basePrice;
+    private String description;
     private Long quantity;
 
 }
