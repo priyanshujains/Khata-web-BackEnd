4497231d2cd9ccdbbe1c10ccbaf325d83023f96f
priyanshujains <jainpriyanshu0129@gmail.com>
2025-05-06 16:01:07 +0530

    fixed merge conflict

--- src/main/java/com/example/KhataWeb/Contoller/CustomerController.java
+++ src/main/java/com/example/KhataWeb/Contoller/CustomerController.java
@@ -1,34 +1,4 @@
-<<<<<<< HEAD
-////package com.example.KhataWeb.Contoller;
-////
-////import org.springframework.beans.factory.annotation.Autowired;
-////import org.springframework.http.ResponseEntity;
-////import org.springframework.web.bind.annotation.*;
-////
-////@RestController()
-////@RequestMapping("/customer")
-////public class CustomerController {
-////
-////    @GetMapping()
-////    public ResponseEntity<?> getAllCustomers(){
-////        return new ResponseEntity<>()
-////    }
-////
-////    @PostMapping()
-////    public ResponseEntity<?> addCustomer(@ResponseBody
-////                                         )
-////
-////    @GetMapping("/{custId}")
-////    public ResponseEntity<?> getProductById(@PathVariable long custId){
-////
-////    }
-////
-////    @
-//
-//
-//
-//}
-=======
+
 package com.example.KhataWeb.Contoller;
 
 import com.example.KhataWeb.Models.Customer;
@@ -102,4 +72,4 @@ public class CustomerController {
 
 
 }
->>>>>>> origin/feature/ishka
+
